751a43ae042a5ea679a959145ace314088ae4c39
Meer Quais <meerquais017@gmail.com>
2024-12-25 17:40:32 +0500

    JavaScript Inc and Dec

---
+++ JavaScript/Class 2/app.js
@@ -0,0 +1,85 @@
+// alert("JS Connected."); 
+
+
+// initialize ----------> var 'variable name';
+// Declariztion -------> = "value";
+
+var myName = "meer";
+
+// console.log(myName);
+
+// reinitialization 
+
+myName = "Meer";
+
+var age = 25;
+
+// legal var names 
+
+var snake_case = "Snake Case";
+var camelCase = "Camel Case";
+var userResponseTime;
+var $name;
+var my_name = "Meer Quais";
+
+// Illegal var names 
+
+// using numbers at the start ----> var 1232var 
+// using special chars ----> @#%^&&*() 
+
+
+// console.log(myName)
+// console.log(age);
+
+
+var add = 2 + 2 ;
+var sub = 4 - 2 ;
+var multiply = 2 * 2 ;
+var division = 4 / 3 ;
+var Modulas = 3 % 2 ;
+
+
+// Increment and Decrement ---> ++ , -- 
+// Post and Pre ---> var++ , ++var , var-- , --var
+
+var number = 2; // 3 // 4
+
+var inc = ++number ; // 3 
+
+var incPost = number++ ; // post increment 3
+
+console.log(inc);
+console.log(number);
+console.log(incPost);
+
+
+
+var a = 15;
+
+
+
+        //  15  + 17   - 16  = 16
+// var value1 = a++ + ++a - --a;
+
+
+//            16 + 17  -  16 + 16  + 15 - 15 - 15 - 15 = 3 
+var value2 = ++a + ++a - --a + a-- + a - a - a++ - --a;
+
+
+console.log(value2)
+
+
+
+
+
+
+
+
+
+
+
+// console.log(add);
+// console.log(sub);
+// console.log(multiply);
+// console.log(division);
+// console.log(Modulas);
