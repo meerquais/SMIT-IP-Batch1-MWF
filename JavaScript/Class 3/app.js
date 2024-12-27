2a297d331893bb4534cb902e67862c26166626c0
muhammadminhaj786 <eb20103087.minhajwahid@gmail.com>
2024-12-27 17:44:50 +0500

    If else

---
+++ JavaScript/Class 3/app.js
@@ -0,0 +1,158 @@
+// alert("JavaScript is Connected.")
+
+// var myName = "Meer" ;
+
+// myName = "Meer Quais" ; 
+// var myClass = "Camel Case" ;
+// var snake_case = "Snake Case" ;
+
+// var $class; 
+// var class1;
+
+// var _name 
+// var 1class; 
+// var @fasdfas ; 
+// console.log(myName)
+
+
+
+// var add = 2 + 2;
+// var sub = 4 - 2;
+// var multifply = 2 * 6;
+// var divide = 30 / 40;
+// var modulas = 40 % 30 ;
+
+
+// console.log("Add" , add)
+// console.log("sub" , sub)
+// console.log("multiply" ,multifply)
+// console.log("Divide" , divide)
+// console.log("Modulas" ,modulas)
+
+// var a = 10 + (20 * 40);
+
+// var result = a--;
+
+// console.log(result)
+// console.log(a)
+
+
+// var firstName = "Meer" ;
+// var lastName = "Quais" ;
+
+// var fullName = firstName + " " + lastName ;
+
+// console.log(fullName)
+
+
+// var val1 = "10";
+// var val2 =  10; 
+
+// var result = val1 - val2 ; 
+
+// console.log(result)
+
+
+// 5 data types
+
+// String ----> "    "
+// Number ----> 123231
+// Boolean ----> true or false
+// undefined ----> undefined
+// null ----> null 
+
+
+// var name;
+// var string = "This is a String DataType";
+// var number = 1;
+// var boolean = true;
+// var undef = undefined; 
+
+// undef = "String"
+// var nothing = null;
+
+
+// var firstName = prompt("Type your First name" , "Mark");
+// var lastName = prompt("type your last name" , "Berg");
+// var age = +prompt("Enter your age" , 18)
+
+// var fullName = firstName + " " + lastName
+
+// console.log(fullName)
+// console.log(age);
+
+
+// if(condition){
+//     piece of code
+// }else{
+//     piece of code 
+// }
+
+
+// var age = 18;
+
+// if(age == 20){
+//     alert("Allowed")
+// }else{
+//     alert("Not Allowed")
+// }
+
+// === ----> Strict Equality ===> DataType aur Value compare karta hai.
+
+ // example: 5 === "5" --> false
+
+// == ----> Equality ===> Value compare karta hai.
+
+// example: 5 == "5" --> true
+
+// != ----> inequality ===> compares values for inequality.
+
+// example : 5 != "5" ==> false
+
+// !== ----> Strict inequality ===> compares values and types for inequality.
+
+// example : 5 !== "5" --> true
+
+
+// < (Less than) : Example : 5 < 10 ===> true
+
+// <= (Less than or equal to) : Example : 5 <= 5 ===> true.
+
+// > (Greater than) : Example : 10 > 5 ===> true 
+
+// >= (Greater than or equal to): Example : 10 >= 10 ===> true.
+
+
+
+// if(age >= 19){
+    //     console.log("Allowed");
+    // }else{
+        //     console.log("Not Allowed.");
+        
+        // }
+        
+        
+// var age = 20;
+
+// if(age >= 25){
+//     console.log("25 allowed.");
+    
+// }else if(age <= 25){
+//     console.log("les than 25 allowed.");
+    
+// }else if(age == 20){
+//         console.log("20 is allowed");
+        
+// }else{
+//     console.log("Not allowed.");
+    
+// }
+        
+
+        
+        
+        
+        
+        
+        
+        
