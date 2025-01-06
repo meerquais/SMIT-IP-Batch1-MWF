85522e9a19f6a0aa9b78dc4ec26022a952ab63a0
Meer Quais <meerquais017@gmail.com>
2025-01-06 17:47:39 +0500

    JavaScript Loops and deployment

---
+++ JavaScript/Class 7/app.js
@@ -0,0 +1,37 @@
+// console.log("Connected.");
+
+
+// var cities = ["Karachi", "Lahore" , "Multan" , "Peshawar" , "Islamabad"]
+
+// for(var i = 0; i < cities.length; i++ ){
+//     console.log(cities[i])
+// }
+
+
+// var fruits = ["Apple" , "Mango" , "Orange"];
+
+// for(var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+    
+// }
+
+// for(var i = 1; i <= 10; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
+
+// for(var i = 1; i <=10; i++){
+//     var stars = " ";
+//     for(var j = 1; j <=i; j++){
+//         stars += "*";
+//     }
+//     console.log(stars)
+// }
+
+
+for(var i = 1; i <= 3; i++){
+    for(var j = 1; j <= 3; j++){
+        console.log("i = " + i , "j = " + j )
+    }
+}
