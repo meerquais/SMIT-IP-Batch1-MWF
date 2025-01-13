8b68c62e8b19640d23a978e0fdf23b4e3a2462e9
Meer Quais <meerquais017@gmail.com>
2025-01-13 17:55:14 +0500

    Functions

---
+++ JavaScript/Class 9/app.js
@@ -0,0 +1,104 @@
+// alert("Connected.")
+
+// function functionName(){
+//     // block of code 
+// }
+
+
+// function addNums(c , b , d){
+//     var sum = c + b + d ;
+//     console.log(sum)
+//     return sum;
+    
+// }
+
+// addNums(40,50 , 22);
+// addNums(40,70 , 20);
+// addNums(40,120, 3);
+
+
+// var result = addNums(50 , 40);
+
+// console.log("Sum: " ,result)
+
+
+// var userName = prompt("Type your name")
+
+
+// function greetUser(name){
+//     console.log("Hello, " + name + "!");
+// }
+
+// greetUser(userName);
+// greetUser("Quais");
+
+// var value1 = +prompt("type value 1");
+// var value2 = +prompt("type value 2");
+
+// function multiplyNumbers(x , y){
+//     return x * y;
+// }
+
+// var result = multiplyNumbers(value1 , value2);
+
+// console.log(result)
+
+// local scope vs global scope // variables 
+
+// local scope , sirf functions ky andar kaam karta hai
+
+
+
+// function calculate(number){
+    //     var square = number * number
+    //     console.log(square)
+    //     return square;
+    // }
+    
+    // console.log(calculate(4));
+    // console.log(square);
+    
+    // var name = "meer quais";
+
+    // function greetUser(a , b){
+    //     var name = "Meer";
+    //     console.log()
+    // }
+
+
+    // var value = 50; // global variable
+
+    // function modifyValue(){
+    //     var value = 10; // local variable 
+    //     console.log("Local Variable Value: " , value)
+    // }
+
+    // modifyValue();
+    // console.log("Global Value: " , value);
+    
+
+// var bal = 1000;
+
+// function withdraw(amount){
+//     if(amount > bal){
+//         console.log("Insufficient balance!")
+//     }
+
+//     bal -= amount; // update global variable 
+
+//     console.log("Withdrawl Successful. New Balance: " , bal);
+
+// }
+
+// withdraw(200); // 800
+// withdraw(900); // insufficient bal
+
+// console.log("remaining Balance: " , bal)
+
+
+/* function greetUser(name = "Guest"){
+    console.log("Hello, " + name + "!")
+}
+
+greetUser("Ali");
+greetUser(); */
