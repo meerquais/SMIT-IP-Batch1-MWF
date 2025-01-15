d7ecb37d573d3b23e44a3c03eb0cfcbcca67b05c
Meer Quais <meerquais017@gmail.com>
2025-01-15 17:46:44 +0500

    Events

---
+++ JavaScript/Class 10/app.js
@@ -0,0 +1,23 @@
+// alert("connected");
+
+
+// function addNumber(a,b){
+
+//     var result = a + b ;
+
+//     return result;
+
+// }
+
+// addNumber(); // calling/trigger function.
+
+// addNumber(55,455);
+
+// console.log(addNumber(2,49));
+
+
+function popUp(){
+    var name = prompt("Type your name")
+    console.log("Welcome " + name)
+}
+
